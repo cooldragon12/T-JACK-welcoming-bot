a0d22cfb5333c36369560ebadfb441701caa376d
Johndel Encabo <johndelencabo@gmail.com>
2021-10-20 15:54:28 +0800

    first commit

---
+++ bot.py
@@ -0,0 +1,35 @@
+
+import discord
+from discord import Client
+intents = discord.Intents.default()
+intents.members=True
+client= Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print("We are Ready!!!")
+    # welcomechannel = await client.fetch_channel("897640623669116968")
+
+@client.event
+async def on_member_join(member):
+    guild = member.guild
+    user = member.name
+    mention = member.mention
+    members = len(list(member.guild.members))
+
+    print(f"{user} has join")
+    msg = f'''Hello {mention}! 
+    Welcome to {guild} and thank you for joining enjoy
+    Current population of this guild is {members}.
+    '''
+    channel = client.get_channel(897640623669116968)
+    
+    embed = discord.Embed(color=discord.Colour.random(), description=msg)
+    embed.set_thumbnail(url=f'{member.avatar_url}')
+    
+    await channel.send(embed=embed)
+
+
+
+client.run('OTAwMDAyOTE0OTQzMjM4MTc0.YW6_CA.OppmM5isjqE1Zg-OikFpw7-HOR4')
