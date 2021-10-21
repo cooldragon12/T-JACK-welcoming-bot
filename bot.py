db6950bbea4ece4b3fdd6189adbd1c57c48227e4
Johndel Encabo <johndelencabo@gmail.com>
2021-10-21 21:18:44 +0800

    test5

--- bot.py
+++ bot.py
@@ -14,8 +14,60 @@ client= Client(intents=intents)
 @client.event
 async def on_ready():
     print("We are Ready!!!")
-    # welcomechannel = await client.fetch_channel("897640623669116968")
+    channel = client.get_channel(900665979812589588)
+    emoji_selection = [":school_satchel:", ":1251squidwardawake:", ":mechanical_arm:", ":1000hahaa:", ":5724duck:", ":8793beluga:"]
 
+    message = await client.send_message(channel, '''
+Welcome to our server T-AJACK
+
+To get your role click the following emojis:
+
+    🎒 = Classmates - if you're are classmates 
+
+    :1251squidwardawake: = Iyah's Katoto - if you're friend of Iyah
+
+    🦾 = Karl's Katoto - if you're friend of Karl
+
+    :1000hahaa: = Shawn's Katoto - if you're friend of Shawn
+
+    :5724duck: = Johndel's Katoto - if you're friend of Johndel
+
+    :8793beluga: = Cassandra's Katoto  - if you're friend of Cassandra
+
+Enjoy the server 
+    ''')
+    await client.add_reaction(message, emoji=emoji_selection[0])
+    await client.add_reaction(message, emoji=emoji_selection[1])
+    await client.add_reaction(message, emoji=emoji_selection[2])
+    await client.add_reaction(message, emoji=emoji_selection[3])
+    await client.add_reaction(message, emoji=emoji_selection[4])
+    await client.add_reaction(message, emoji=emoji_selection[5])
+
+@client.event
+async def on_reaction_add(reaction,user):
+        reaction = await client.wait_for_reaction(emoji="🏃", message=message)
+        emoji_selection = [":school_satchel:", ":1251squidwardawake:", ":mechanical_arm:", ":1000hahaa:", ":5724duck:", ":8793beluga:"]
+        channel = client.get_channel(900665979812589588)
+        if reaction.message.channel.id != channel:
+            return
+        emoji = reaction.emoji
+        if emoji == emoji_selection[0]:
+            role = get(user.server.roles, name="Classmates")
+            
+        elif emoji == emoji_selection[1]:
+            role = get(user.server.roles, name="IYAH's Katoto")
+            
+        elif emoji == emoji_selection[2]:
+            role = get(user.server.roles, name="KARL's Katoto")
+                
+        elif emoji == emoji_selection[3]:
+            role = get(user.server.roles, name="SHAWN's Katoto")
+                
+        elif emoji == emoji_selection[4]:
+            role = get(user.server.roles, name="JOHNDEL's Katoto")
+        elif emoji == emoji_selection[5]:
+            role = get(user.server.roles, name="CASSANDRA's Katoto")
+        await client.add_roles(reaction.message.author, role)    
 @client.event
 async def on_member_join(member):
     guild = member.guild
@@ -36,32 +88,7 @@ async def on_member_join(member):
     await channel.send(embed=embed)
 # async def get_emoji(guild: discord.Guild, arg):
 #     return get(ctx.guild.emojis, name=arg)
-@client.event
-async def on_reaction_add(reaction, user):
-    emoji_selection = [":school_satchel:", ":1251squidwardawake:", ":mechanical_arm:", ":1000hahaa:", ":5724duck:", ":8793beluga:"]
 
-    channel = client.get_channel(900665979812589588)
-    emoji = reaction.emoji
-
-    if reaction.message.id == (900669947770970122):
-        if emoji == emoji_selection[0]:
-            role = get(user.server.roles, name="Classmates")
-            await client.add_roles(user, role)
-        elif emoji == emoji_selection[1]:
-            role = get(user.server.roles, name="IYAH's Katoto")
-            await client.add_roles(user, role)
-        elif emoji == emoji_selection[2]:
-            role = get(user.server.roles, name="KARL's Katoto")
-            await client.add_roles(user, role)    
-        elif emoji == emoji_selection[3]:
-            role = get(user.server.roles, name="SHAWN's Katoto")
-            await client.add_roles(user, role)    
-        elif emoji == emoji_selection[4]:
-            role = get(user.server.roles, name="JOHNDEL's Katoto")
-            await client.add_roles(user, role)    
-        elif emoji == emoji_selection[5]:
-            role = get(user.server.roles, name="CASSANDRA's Katoto")
-            await client.add_roles(user, role)    
 
 client.run(os.environ.get('TOKEN_SECRET'))
 
